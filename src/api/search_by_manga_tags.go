7858497ae1d0cc32f4c70a5f2bfa010245285d33
leviathan <gonzalezsaul31@gmail.com>
2024-08-26 18:45:12 -0600

    made the tags struct and will now test the api to search by tags

--- src/api/search_by_manga_tags.go
+++ src/api/search_by_manga_tags.go
@@ -1,2 +1,29 @@
-// this is to make calls to get manga tags etc.
+// this is to make api calls to get mangas by searching based on tags etc.
 package api
+
+import "github.com/google/uuid"
+
+type Tags struct {
+	Data []struct {
+		ID   uuid.UUID `json:"id"`
+		Type string    `json:"type"`
+
+		Attributes struct {
+			Name        map[string]string `json:"name"`
+			Description map[string]string `json:"description"`
+			Group       string            `json:"group"`
+			Version     int               `json:"version"`
+		} `json:"attributes"`
+
+		Relationship []struct {
+			ID         uuid.UUID `json:"id"`
+			Type       string    `json:"type"`
+			Related    string    `json:"related"`
+			Attributes struct{}  `json:"attributes"`
+		}
+
+		Limit  int `json:"limit"`
+		Offset int `json:"offset"`
+		Total  int `json:"total"`
+	} `json:"data"`
+}

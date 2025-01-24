09132d62570314ee08aeaf87fe40e8a6f7fb79dd
Saul Gonzalez <gonzalezsaul31@gmail.com>
2025-01-24 14:51:24 -0700

    right now i need to learn how to do a proper http client, so im gonna study up on some things first before i right it up.

--- src/api/api_client.go
+++ src/api/api_client.go
@@ -1,1 +1,16 @@
 package api
+
+import (
+	"context"
+	"net/http"
+)
+
+const baseURL string = "https://api.mangadex.com"
+
+type MangadexService struct {
+	client *http.Client
+	header http.Header
+}
+
+func DexClient() *MangadexService {
+}

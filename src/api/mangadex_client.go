8854bb3cad0bd1aec9146c26bf4129bfb1fdb957
Saul Gonzalez <gonzalezsaul31@gmail.com>
2025-01-30 19:39:03 -0700

    created wrapper for http request and decoding to json response

--- src/api/mangadex_client.go
+++ src/api/mangadex_client.go
@@ -2,6 +2,8 @@ package api
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
 )
@@ -30,8 +32,39 @@ func DexClient() *MangadexService {
 	}
 }
 
-func request(ctx context.Context, methodCode, url string, body io.Reader) (*http.Response, error) {
+func (c *MangadexService) Request(ctx context.Context, methodCode, url string, body io.Reader) (*http.Response, error) {
+	req, err := http.NewRequestWithContext(ctx, methodCode, url, body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create request: %w", err)
+	}
+
+	req.Header = c.header
+
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("failed to do request: %w", err)
+	} else if resp.StatusCode != http.StatusOK {
+		var er ErrorResponse
+		if err = json.NewDecoder(resp.Body).Decode(&er); err != nil {
+			return nil, err
+		}
+		defer resp.Body.Close()
+		return nil, fmt.Errorf("non 200 status code from mangadex server: %d %s", resp.StatusCode, er.GetErrors())
+	}
+	return resp, nil
 }
 
-func decodeJson() {
+// (t any) represents the structs for "type" manga data, author data and cover image data. Hopefully it works the way i think it does
+func (c *MangadexService) RequestAndDecodeJson(ctx context.Context, method, url string, body io.Reader, t any) error {
+	resp, err := c.Request(ctx, method, url, body)
+	if err != nil {
+		return fmt.Errorf("request failed: %w", err)
+	}
+	defer resp.Body.Close()
+
+	if err := json.NewDecoder(resp.Body).Decode(t); err != nil {
+		return fmt.Errorf("failed to decode JSON: %w", err)
+	}
+
+	return nil
 }
